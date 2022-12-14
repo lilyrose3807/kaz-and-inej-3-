18c3edd7e1363b88da6a62e6d6c65c834fbd8720
lilyrose3807 <110089302+lilyrose3807@users.noreply.github.com>
2022-12-14 16:47:59 +1100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+
+function setup() {
+  createCanvas(400,400);
+  student1 = new Student ("Kaz",18,10)
+  student1.display() 
+
+  student2 = new Student ("Inej",16,9)
+  student2. display() 
+
+}
+
+function draw() 
+{
+  background(30);
+  
+}
